6b9b2afc12f8d8542e1050db9e84b487548ed056
nilson <n.parram@uniandes.edu.co>
2021-04-25 16:36:35 -0500

    Agregar modulo Albumes en app-module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front'`, () => {
+  /*it(`should have as title 'VinylApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front');
-  });
+    expect(app.title).toEqual('Vinyl App');
+  });*/
 
-  it('should render title', () => {
+  /*it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('front app is running!');
-  });
+  });*/
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumModule } from './album/album.module';
+import { AlbumListarComponent } from './album/album-listar/album-listar.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlbumModule
   ],
   providers: [],
   bootstrap: [AppComponent]
