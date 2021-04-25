21df5958d8fc052264f32356ac82cabf21974f39
nilson <n.parram@uniandes.edu.co>
2021-04-25 11:40:19 -0500

    Crear componente listar albumes, prueba para componente y html .

--- src/app/album/album.module.ts
+++ src/app/album/album.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlbumComponent } from './album.component';
+import { AlbumListarComponent } from './album-listar/album-listar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AlbumComponent]
+  declarations: [
+    AlbumComponent,
+    AlbumListarComponent
+  ],
+  exports: [AlbumListarComponent]
 })
 export class AlbumModule { }
