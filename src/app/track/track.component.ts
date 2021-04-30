cd1723e1ecd04b5e33e772d92f2051909c223cb9
Alberto Chica <a.chica@uniandes.edu.co>
2021-04-30 11:15:20 -0500

    Actualizacion track.component.css

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
-  styleUrls: ['./track.component.scss']
+  styleUrls: ['./track.component.css']
 })
 export class TrackComponent implements OnInit {
 
