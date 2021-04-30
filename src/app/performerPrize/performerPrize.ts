5104bc6e21f5a0e2bb4c2c7b493bf66088936763
Ricardo Matiz <rmatizc@uniandes.edu.co>
2021-04-30 11:09:31 -0500

    Creacion componente performerPrize

---
+++ src/app/performerPrize/performerPrize.ts
@@ -0,0 +1,15 @@
+export class PerformerPrize {
+  id: number;
+  premiationDate: Date;
+
+
+  constructor(
+    id: number,
+    premiationDate: Date
+
+  )
+  {
+   this.id = id;
+   this.premiationDate = premiationDate;
+ }
+ }
