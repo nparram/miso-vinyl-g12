8e2424d4c17e73c30efcc063d79ab58e279b6431
Alberto Chica <a.chica@uniandes.edu.co>
2021-04-29 19:49:46 -0500

    Implementacion Performer-Detail

--- src/app/performer/performer.module.ts
+++ src/app/performer/performer.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PerformerListarComponent } from './performer-listar/performer-listar.component';
 import {HttpClientModule} from '@angular/common/http';
+import { PerformerDetailComponent } from './performer-detail/performer-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [PerformerListarComponent],
+  declarations: [PerformerListarComponent,PerformerDetailComponent],
   exports: [PerformerListarComponent],
 })
 export class PerformerModule { }
