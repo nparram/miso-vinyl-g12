cf79b636054504ff2989357509ed51bef35a5e31
Ricardo Matiz <rmatizc@uniandes.edu.co>
2021-05-08 12:28:47 -0500

    Integracion Prize en principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AlbumListarComponent } from './album/album-listar/album-listar.component';
 import { PerformerListarComponent } from './performer/performer-listar/performer-listar.component';
+import { PrizeListarComponent } from './prize/prize-listar/prize-listar.component';
 
 const routes: Routes = [
   { path: 'albums/list', component: AlbumListarComponent } ,
-  { path: 'performers/list', component: PerformerListarComponent }
+  { path: 'performers/list', component: PerformerListarComponent },
+  { path: 'prizes/list', component: PrizeListarComponent }
 ];
 
 @NgModule({
