63d9604efad7d15e980a1240975fa2e7fefc3dcf
nilson <n.parram@uniandes.edu.co>
2021-05-15 18:58:48 -0500

    Manejo de errores en servicio Listar Albumes

--- src/app/album/album.service.ts
+++ src/app/album/album.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { AlbumDetail } from './albumDetail';
-
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +14,13 @@ export class AlbumService {
 
   constructor(private http: HttpClient) { }
 
-  getAlbums(): Observable<AlbumDetail[]> {
-    return this.http.get<AlbumDetail[]>(this.apiUrl);
+  getAlbums(): Observable<Array<AlbumDetail>> {
+    //ToDo Array<AlbumDetail>
+    return this.http.get<Array<AlbumDetail>>(this.apiUrl).pipe(
+      catchError(err => {
+        console.log(`En el servicio: ${err.error}`);
+        return throwError(err);
+      }));
   }
 
 }
